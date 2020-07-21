3c9a4cb3a367be11dd38509f8e4f538f5fe26ddd
Timothée Poisot <timothee.poisot@umontreal.ca>
2020-07-21 15:07:00 -0400

    add a function to create the assets folder

--- src/SimpleSDMDataSources.jl
+++ src/SimpleSDMDataSources.jl
@@ -1,5 +1,16 @@
 module SimpleSDMDataSources
 
-greet() = print("Hello World!")
+# Load the dependencies for this package
+using ArchGDAL
+using HTTP
+using ZipFile
+
+function _raster_assets_folder()
+    project_path = dirname(something(Base.current_project(pwd()), Base.load_path_expand(LOAD_PATH[2])))
+    assets_folder = joinpath(project_path, "assets")
+    ispath(assets_folder) || mkdir(assets_folder)
+    return assets_folder
+end
+
 
 end # module
