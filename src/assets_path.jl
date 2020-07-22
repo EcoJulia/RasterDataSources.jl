444a9cefcaba337c9edd537f528fb21dcba0f457
Timothée Poisot <timothee.poisot@umontreal.ca>
2020-07-22 16:43:47 -0400

    add a function to remove rasters

--- src/assets_path.jl
+++ src/assets_path.jl
@@ -11,3 +11,12 @@ function _raster_assets_folder(::Type{TS}, ::Type{TD}) where {TS <: SDMDataSourc
     ispath(assets_folder) || mkpath(assets_folder)
     return assets_folder
 end
+
+function cleanup_assets()
+    ispath(_raster_assets_folder()) && rm(_raster_assets_folder(); recursive=false)
+end
+
+function cleanup_assets(::Type{TS}, ::Type{TD}) where {TS <: SDMDataSource, TD <: SDMDataSet}
+    ispath(_raster_assets_folder(TS, TD)) && rm(_raster_assets_folder(TS, TD); recursive=false)
+end
+
