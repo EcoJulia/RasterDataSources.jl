8beda5043724580f46597e534fc175143685a2e2
Rafael Schouten <rafaelschouten@gmail.com>
2020-09-24 19:18:53 +1000

    update deps and exports

--- src/SimpleSDMDataSources.jl
+++ src/SimpleSDMDataSources.jl
@@ -1,26 +1,31 @@
 module SimpleSDMDataSources
 
 # Load the dependencies for this package
-using HTTP
-using ZipFile
+using HTTP,
+      ZipFile,
+      Dates
 
 # Abstract types for the download
 abstract type SDMDataSource end
 abstract type SDMDataSet end
 
 # List of data sources
-struct WorldClim <: SDMDataSource end
-struct CHELSA <: SDMDataSource end
 struct EarthEnv <: SDMDataSource end
-
-export WorldClim, CHELSA, EarthEnv
-
-# List of data sets
+struct CHELSA <: SDMDataSource end
+struct Weather <: SDMDataSet end
 struct BioClim <: SDMDataSet end
 struct LandCover <: SDMDataSet end
 struct HabitatHeterogeneity <: SDMDataSet end
 
-export BioClim, LandCover, HabitatHeterogeneity
+export WorldClim, CHELSA, EarthEnv, AWAP, ALWB
+
+export BioClim, Weather, LandCover, HabitatHeterogeneity
+
+# Using types to specify layers makes sense in some contexts
+# These are exported for AWAP, but are experimental
+export Temperature, VapourPressure, Solar, Rainfall, H09, H15, MinAve, MaxAve
+
+export download_raster, rasterpath
 
 # Create a path for the various assets
 include("assets_path.jl")
@@ -29,10 +34,14 @@ include("assets_path.jl")
 include("download.jl")
 
 # Download raster data
+include("worldclim/shared.jl")
 include("worldclim/bioclim.jl")
+include("worldclim/weather.jl")
 include("chelsa/bioclim.jl")
 include("earthenv/landcover.jl")
 include("earthenv/habitatheterogeneity.jl")
+include("awap/awap.jl")
+include("alwb/alwb.jl")
 
 export download_raster
 
