38d60ec08976cf0916ecc3c3698b145de27b5a0b
Rafael Schouten <rafaelschouten@gmail.com>
2020-09-24 19:13:45 +1000

    use ENV for assets path

--- src/assets_path.jl
+++ src/assets_path.jl
@@ -1,19 +1,35 @@
-function _raster_assets_folder()
-    project_path = dirname(something(Base.current_project(pwd()), Base.load_path_expand(LOAD_PATH[2])))
-    assets_folder = joinpath(project_path, "assets", "general")
-    ispath(assets_folder) || mkpath(assets_folder)
-    return assets_folder
+# Allow this to be set manually
+function rasterpath() 
+    if haskey(ENV, "ECODATASOURCES_PATH") && isdir(ENV["ECODATASOURCES_PATH"])
+        ENV["ECODATASOURCES_PATH"]
+    else
+        error("You must set `ENV[\"ECODATASOURCES_PATH\"]` to a path in your system")
+    end
 end
 
-function _raster_assets_folder(::Type{TS}, ::Type{TD}) where {TS <: SDMDataSource, TD <: SDMDataSet}
-    project_path = dirname(something(Base.current_project(pwd()), Base.load_path_expand(LOAD_PATH[2])))
-    assets_folder = joinpath(project_path, "assets", string(TS), string(TD))
-    ispath(assets_folder) || mkpath(assets_folder)
-    return assets_folder
-end
+# function _raster_assets_folder()
+#     project_path = dirname(something(Base.current_project(pwd()), Base.load_path_expand(LOAD_PATH[2])))
+#     assets_folder = joinpath(project_path, "assets", "general")
+#     ispath(assets_folder) || mkpath(assets_folder)
+#     return assets_folder
+# end
+
+# The folder structure could be more or less deeply nested than this
+
+# function _raster_assets_folder(::Type{TS}, ::Type{TD}) where {TS <: SDMDataSource, TD <: SDMDataSet}
+#     project_path = dirname(something(Base.current_project(pwd()), Base.load_path_expand(LOAD_PATH[2])))
+#     assets_folder = joinpath(project_path, "assets", string(TS), string(TD))
+#     ispath(assets_folder) || mkpath(assets_folder)
+#     return assets_folder
+# end
 
 function cleanup_assets()
-    ispath(_raster_assets_folder()) && rm(_raster_assets_folder(); recursive=false)
+    # May need an "are you sure"? - this could be a lot of GB of data to lose
+    ispath(rasterpath()) && rm(rasterpath())
+end
+
+function cleanup_assets(T::Type)
+    ispath(rasterpath(T)) && rm(rasterpath(T))
 end
 
 function cleanup_assets(::Type{TS}, ::Type{TD}) where {TS <: SDMDataSource, TD <: SDMDataSet}
