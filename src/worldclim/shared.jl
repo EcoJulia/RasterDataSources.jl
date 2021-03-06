962bdcfcedd7344e969c1b92036f9fd6d23f8244
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-03-06 16:13:19 -0500

    remove additional worldclim files

--- src/worldclim/shared.jl
+++ src/worldclim/shared.jl
@@ -17,32 +17,3 @@ rasterpath(T::Type{<:WorldClim}, layer; kw...) =
     joinpath(rasterpath(T), string(layer), rastername(T, layer; kw...))
 
 _zipfile_to_read(raster_name, zf) = first(filter(f -> f.name == raster_name, zf.files))
-
-_format(::Type{WorldClim}, ::Type{SSP126}) = "ssp126"
-_format(::Type{WorldClim}, ::Type{SSP245}) = "ssp245"
-_format(::Type{WorldClim}, ::Type{SSP370}) = "ssp370"
-_format(::Type{WorldClim}, ::Type{SSP585}) = "ssp585"
-
-_format(::Type{WorldClim}, ::Type{BCCCSM2MR}) = "BCC-CSM2-MR"
-_format(::Type{WorldClim}, ::Type{CNRMCM61}) = "CNRM-CM6-1"
-_format(::Type{WorldClim}, ::Type{CNRMESM21}) = "CNRM-ESM2-1"
-_format(::Type{WorldClim}, ::Type{CanESM5}) = "CanESM5"
-_format(::Type{WorldClim}, ::Type{GFDLESM4}) = "GFDL-ESM4"
-_format(::Type{WorldClim}, ::Type{IPSLCM6ALR}) = "IPSL-CM6A-LR"
-_format(::Type{WorldClim}, ::Type{MIROCES2L}) = "MIROC-ES2L"
-_format(::Type{WorldClim}, ::Type{MIROC6}) = "MIROC6"
-_format(::Type{WorldClim}, ::Type{MRIESM2}) = "MRI-ESM2-0"
-
-function _format(::Type{WorldClim}, date::Year)
-    if date == Year(2030)
-        return "2021-2040"
-    elseif date == Year(2050)
-        return "2041-2060"
-    elseif date == Year(2070)
-        return "2061-2080"
-    elseif date == year(2090)
-        return "2081-2100"
-    end
-    @warn "Wrong date for future worldclim, returning 2030"
-    return "2021-2040"
-end
