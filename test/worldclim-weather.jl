7cd3a0955ac299e20b34348b2dbe517fa2700324
Rafael Schouten <rafaelschouten@gmail.com>
2021-05-04 02:36:51 +1000

    test worldclim weather locally

--- test/worldclim-weather.jl
+++ test/worldclim-weather.jl
@@ -1,6 +1,6 @@
 @testset "WorldClim Weather" begin
 
-    using RasterDataSources: rastername, zipurl, zipname, zippath
+    using RasterDataSources: rastername, rasterpath, zipurl, zipname, zippath
 
     raster_file = joinpath(ENV["RASTERDATASOURCES_PATH"], "WorldClim", "Weather", "prec", "wc2.1_2.5m_prec_2001-01.tif")
     @test rasterpath(WorldClim{Weather}, :prec; date=Date(2001, 1)) == raster_file
@@ -13,7 +13,8 @@
     @test zipname(WorldClim{Weather}, :prec; decade=Date(2010)) == 
         "wc2.1_2.5m_prec_2010-2018.zip"
 
-    # These files are 3GB each. Probably too big to test in CI.
-    # Not sure what to do about that.
-    # getraster(WorldClim{Weather}, :prec; dates=Date(2001):Month(1):Date(2001, 12))
+    # These files are 3GB each. Too big to test in CI.
+    if !haskey(ENV, "CI")
+        getraster(WorldClim{Weather}, :prec; date=Date(2001):Month(1):Date(2001, 12))
+    end
 end
