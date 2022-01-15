6a79e9184fb0badc96a601c5e528e9481c1fe05f
Penn <jenkspt@gmail.com>
2022-01-14 16:43:14 -0800

    add keyword arguments

--- test/srtm.jl
+++ test/srtm.jl
@@ -7,20 +7,20 @@ using RasterDataSources: rasterpath, zipurl, zipname
     zip_url = URI(scheme = "https", host = "srtm.csi.cgiar.org", path = "/wp-content/uploads/files/srtm_5x5/TIFF/srtm_01_02.zip")
     tile_index1 = CartesianIndex(2, 1)   # [y, x] order
     tile_index2 = CartesianIndex(2, 2)   # [y, x] order
-    @test zipurl(SRTM, tile_index1) == zip_url
-    @test zipurl(SRTM, tile_index1) == zip_url
-    @test zipname(SRTM, tile_index1) == "srtm_01_02.zip"
+    @test zipurl(SRTM, tile_index = tile_index1) == zip_url
+    @test zipurl(SRTM, tile_index = tile_index1) == zip_url
+    @test zipname(SRTM, tile_index = tile_index1) == "srtm_01_02.zip"
 
     raster_path1 = joinpath(ENV["RASTERDATASOURCES_PATH"], "SRTM", "srtm_01_02.tif")
     raster_path2 = joinpath(ENV["RASTERDATASOURCES_PATH"], "SRTM", "srtm_02_02.tif")
-    @test rasterpath(SRTM, tile_index1) == raster_path1
-    @test getraster(SRTM, tile_index1) == raster_path1
+    @test rasterpath(SRTM, tile_index = tile_index1) == raster_path1
+    @test getraster(SRTM, tile_index = tile_index1) == raster_path1
     @test isfile(raster_path1)
 
     lon1, lat1 = -180, 55       # Coordinates of [0, 0] pixel of tile x=1, y=2
-    @test RasterDataSources.wgs84_to_tile_index(lon1, lat1) == tile_index1
-    @test getraster(SRTM, (lon1, lat1, lon1, lat1)) == [raster_path1;;]
+    @test RasterDataSources._wgs84_to_tile_index(lon1, lat1) == tile_index1
+    @test getraster(SRTM, bounds = (lon1, lat1, lon1, lat1)) == reshape([raster_path1], 1, 1)
     lon2, lat2 = -172.5, 52.5   # Coordinates of [3000, 3000] pixel of tile x=2, y=2
-    @test RasterDataSources.wgs84_to_tile_index(lon2, lat2) == tile_index2
-    @test getraster(SRTM, (lon1, lat1, lon2, lat2)) == [raster_path1;; raster_path2]
+    @test RasterDataSources._wgs84_to_tile_index(lon2, lat2) == tile_index2
+    @test getraster(SRTM, bounds = (lon1, lat1, lon2, lat2)) == reshape([raster_path1, raster_path2], 1, 2)
 end
