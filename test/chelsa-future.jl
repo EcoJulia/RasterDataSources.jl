eb3f8c0cedd19dfe8bb282b706a9f80db7d87888
tiemvanderdeure <tiemvanderdeure@gmail.com>
2024-10-02 16:26:20 +0200

    update chelsa future test

--- test/chelsa-future.jl
+++ test/chelsa-future.jl
@@ -19,25 +19,25 @@ end
 
 @testset "CHELSA Future BioClim CMIP6" begin
     bioclim_name = "CHELSA_bio5_2041-2070_mri-esm2-0_ssp126_V.2.1.tif"
-    @test rastername(CHELSA{Future{BioClim,CMIP6,MRIESM2,SSP126}}, 5; date=Date(2050)) == bioclim_name
-    bioclim_path = joinpath(ENV["RASTERDATASOURCES_PATH"], "CHELSA", "Future", "BioClim", "SSP126", "MRIESM2")
-    @test rasterpath(CHELSA{Future{BioClim,CMIP6,MRIESM2,SSP126}}) == bioclim_path
+    @test rastername(CHELSA{Future{BioClim,CMIP6,MRI_ESM2_0,SSP126}}, 5; date=Date(2050)) == bioclim_name
+    bioclim_path = joinpath(ENV["RASTERDATASOURCES_PATH"], "CHELSA", "Future", "BioClim", "SSP126", "mri-esm2-0")
+    @test rasterpath(CHELSA{Future{BioClim,CMIP6,MRI_ESM2_0,SSP126}}) == bioclim_path
 
     raster_path = joinpath(bioclim_path, bioclim_name)
     raster_path2 = joinpath(bioclim_path, "CHELSA_bio5_2071-2100_mri-esm2-0_ssp126_V.2.1.tif")
-    @test getraster(CHELSA{Future{BioClim,CMIP6,MRIESM2,SSP126}}, 5; date=Date(2050)) == raster_path
-    @test getraster(CHELSA{Future{BioClim,CMIP6,MRIESM2,SSP126}}, (5,); date=Date(2050)) == (bio5=raster_path,)
-    @test getraster(CHELSA{Future{BioClim,CMIP6,MRIESM2,SSP126}}, [5]; date=Date(2050)) == (bio5=raster_path,)
-    @test getraster(CHELSA{Future{BioClim,CMIP6,MRIESM2,SSP126}}, (5,); date=[Date(2050)]) == 
+    @test getraster(CHELSA{Future{BioClim,CMIP6,MRI_ESM2_0,SSP126}}, 5; date=Date(2050)) == raster_path
+    @test getraster(CHELSA{Future{BioClim,CMIP6,MRI_ESM2_0,SSP126}}, (5,); date=Date(2050)) == (bio5=raster_path,)
+    @test getraster(CHELSA{Future{BioClim,CMIP6,MRI_ESM2_0,SSP126}}, [5]; date=Date(2050)) == (bio5=raster_path,)
+    @test getraster(CHELSA{Future{BioClim,CMIP6,MRI_ESM2_0,SSP126}}, (5,); date=[Date(2050)]) == 
         [(bio5=raster_path,)]
-    @test getraster(CHELSA{Future{BioClim,CMIP6,MRIESM2,SSP126}}, :bio5; date=Date(2050)) == raster_path
-    @test getraster(CHELSA{Future{BioClim,CMIP6,MRIESM2,SSP126}}, (:bio5,); date=Date(2050)) == (bio5=raster_path,)
-    @test getraster(CHELSA{Future{BioClimPlus,CMIP6,MRIESM2,SSP126}}, :bio5; date=Date(2050)) == raster_path
-    @test getraster(CHELSA{Future{BioClimPlus,CMIP6,MRIESM2,SSP126}}, (:bio5,); date=Date(2050)) == (bio5=raster_path,)
+    @test getraster(CHELSA{Future{BioClim,CMIP6,MRI_ESM2_0,SSP126}}, :bio5; date=Date(2050)) == raster_path
+    @test getraster(CHELSA{Future{BioClim,CMIP6,MRI_ESM2_0,SSP126}}, (:bio5,); date=Date(2050)) == (bio5=raster_path,)
+    @test getraster(CHELSA{Future{BioClimPlus,CMIP6,MRI_ESM2_0,SSP126}}, :bio5; date=Date(2050)) == raster_path
+    @test getraster(CHELSA{Future{BioClimPlus,CMIP6,MRI_ESM2_0,SSP126}}, (:bio5,); date=Date(2050)) == (bio5=raster_path,)
     @test isfile(raster_path)
 
     # bioclimplus requires symbol input
-    @test_throws ArgumentError getraster(CHELSA{Future{BioClimPlus,CMIP6,MRIESM2,SSP126}}, 5; date=Date(2050))
+    @test_throws ArgumentError getraster(CHELSA{Future{BioClimPlus,CMIP6,MRI_ESM2_0,SSP126}}, 5; date=Date(2050))
 end
 
 @testset "CHELSA Future Climate CMIP5" begin
@@ -60,33 +60,33 @@ end
     @test isfile(raster_path)
     @test RasterDataSources.getraster_keywords(CHELSA{Future{Climate}}) == (:date, :month)
 end
-
+@edit rasterpath(CHELSA{Future{Climate,CMIP6,GFDL_ESM4,SSP585}})
 @testset "CHELSA Future Climate CMIP6" begin
     date_name =  "CHELSA_gfdl-esm4_r1i1p1f1_w5e5_ssp585_tas_01_2011_2040_norm.tif"
     date_name2 = "CHELSA_gfdl-esm4_r1i1p1f1_w5e5_ssp585_tas_01_2041_2070_norm.tif"
     date_name3 = "CHELSA_gfdl-esm4_r1i1p1f1_w5e5_ssp585_tas_01_2071_2100_norm.tif"
     month_name =  "CHELSA_gfdl-esm4_r1i1p1f1_w5e5_ssp585_tas_01_2011_2040_norm.tif"
     month_name2 = "CHELSA_gfdl-esm4_r1i1p1f1_w5e5_ssp585_tas_02_2011_2040_norm.tif"
-    @test rastername(CHELSA{Future{Climate,CMIP6,GFDLESM4,SSP585}}, :temp; date=Date(2030), month=1) == date_name
+    @test rastername(CHELSA{Future{Climate,CMIP6,GFDL_ESM4,SSP585}}, :temp; date=Date(2030), month=1) == date_name
 
     climate_path = joinpath(ENV["RASTERDATASOURCES_PATH"], "CHELSA", "Future", "Climate", "SSP585", "GFDLESM4")
-    @test rasterpath(CHELSA{Future{Climate,CMIP6,GFDLESM4,SSP585}}) == climate_path
+    @test rasterpath(CHELSA{Future{Climate,CMIP6,GFDL_ESM4,SSP585}}) == climate_path
     date_path = joinpath(climate_path, date_name)
     date_path2 = joinpath(climate_path, date_name2)
     date_path3 = joinpath(climate_path, date_name3)
     month_path = joinpath(climate_path, month_name)
     month_path2 = joinpath(climate_path, month_name2)
-    @test rasterpath(CHELSA{Future{Climate,CMIP6,GFDLESM4,SSP585}}, :temp; date=Date(2030), month=1) == date_path
-    @test rasterpath(CHELSA{Future{Climate,CMIP6,GFDLESM4,SSP585}}, :temp; date=Date(2030), month=1) == date_path
+    @test rasterpath(CHELSA{Future{Climate,CMIP6,GFDL_ESM4,SSP585}}, :temp; date=Date(2030), month=1) == date_path
+    @test rasterpath(CHELSA{Future{Climate,CMIP6,GFDL_ESM4,SSP585}}, :temp; date=Date(2030), month=1) == date_path
     date_url = "https://os.zhdk.cloud.switch.ch/chelsav2/GLOBAL/climatologies/2011-2040/GFDL-ESM4/ssp585/tas/" * date_name
-    @test rasterurl(CHELSA{Future{Climate,CMIP6,GFDLESM4,SSP585}}, :temp; date=Date(2030), month=1) |> string == date_url
-    @test getraster(CHELSA{Future{Climate,CMIP6,GFDLESM4,SSP585}}, :temp; date=Date(2030), month=1) == date_path
-    @test getraster(CHELSA{Future{Climate,CMIP6,GFDLESM4,SSP585}}, (:temp,); date=Date(2030), month=1) == (temp=date_path,)
-    @test getraster(CHELSA{Future{Climate,CMIP6,GFDLESM4,SSP585}}, :temp; date=Date(2030), month=1) == date_path
+    @test rasterurl(CHELSA{Future{Climate,CMIP6,GFDL_ESM4,SSP585}}, :temp; date=Date(2030), month=1) |> string == date_url
+    @test getraster(CHELSA{Future{Climate,CMIP6,GFDL_ESM4,SSP585}}, :temp; date=Date(2030), month=1) == date_path
+    @test getraster(CHELSA{Future{Climate,CMIP6,GFDL_ESM4,SSP585}}, (:temp,); date=Date(2030), month=1) == (temp=date_path,)
+    @test getraster(CHELSA{Future{Climate,CMIP6,GFDL_ESM4,SSP585}}, :temp; date=Date(2030), month=1) == date_path
     # Month is the inner vector
-    @test getraster(CHELSA{Future{Climate,CMIP6,GFDLESM4,SSP585}}, [:temp]; date=[Date(2030)], month=1:2) == 
+    @test getraster(CHELSA{Future{Climate,CMIP6,GFDL_ESM4,SSP585}}, [:temp]; date=[Date(2030)], month=1:2) == 
         [[(temp=month_path,), (temp=month_path2,)]]
-    @test getraster(CHELSA{Future{Climate,CMIP6,GFDLESM4,SSP585}}, [:temp]; date=(Date(2030), Date(2090)), month=1:1) == 
+    @test getraster(CHELSA{Future{Climate,CMIP6,GFDL_ESM4,SSP585}}, [:temp]; date=(Date(2030), Date(2090)), month=1:1) == 
         [[(temp=date_path,)], [(temp=date_path2,)], [(temp=date_path3,)]]
 
     @test isfile(date_path)
