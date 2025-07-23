328057b49703d0f6d542afc8f48d7b2305f079bc
tiemvanderdeure <tiemvanderdeure@gmail.com>
2025-07-23 17:57:23 +0200

    move Future docstring to after where models are added to CMIP5_MODELS

--- src/interface.jl
+++ src/interface.jl
@@ -229,3 +229,48 @@ Download [`CHELSA`](@ref) [`BioClim`](@ref) data from [chelsa-climate.org](https
     
 """
 CHELSA
+
+"""
+    Future{<:RasterDataSet,<:CMIPphase,<:ClimateModel,<:ClimateScenario}
+
+Future climate datasets specified with a dataset, phase, model, and scenario.
+
+## Type Parameters
+
+#### `RasterDataSet`
+
+Currently [`BioClim`](@ref) and [`Climate`](@ref) are implemented
+for [`CHELSA`](@ref) and [`WorldClim`](@ref). Future WorldClim is only available 
+for CMIP6.
+
+#### `CMIPphase`
+
+Can be either [`CMIP5`](@ref) or [`CMIP6`](@ref).
+
+#### `ClimateModel`
+
+Climate models can be chosen from: 
+
+`$(join(CMIP5_MODELS, "`,  `"))` for `CMIP5`;
+
+`$(join(CMIP6_MODELS, "`,  `"))` for `CMIP6`;"
+
+#### `ClimateScenario`
+
+CMIP5 Climate scenarios are all [`RepresentativeConcentrationPathway`](@ref)
+and can be chosen from: `RCP26`, `RCP45`, `RCP60`, `RCP85`
+
+CMIP6 Climate scenarios are all [`SharedSocioeconomicPathway`](@ref) and
+can be chosen from: `SSP126`, `SSP245`, `SSP370`, `SSP585`
+
+However, note that not all climate scenarios are available for all models.
+
+## Example
+
+```jldoctest future
+using RasterDataSources
+datasource = CHELSA{Future{BioClim, CMIP5, GFDL_ESM4, SSP370}}
+```
+
+"""
+Future
--- src/types.jl
+++ src/types.jl
@@ -80,55 +80,6 @@ struct SSP245 <: SharedSocioeconomicPathway end
 struct SSP370 <: SharedSocioeconomicPathway end
 struct SSP585 <: SharedSocioeconomicPathway end
 
-"""
-    Future{<:RasterDataSet,<:CMIPphase,<:ClimateModel,<:ClimateScenario}
-
-Future climate datasets specified with a dataset, phase, model, and scenario.
-
-## Type Parameters
-
-#### `RasterDataSet`
-
-Currently [`BioClim`](@ref) and [`Climate`](@ref) are implemented
-for the [`CHELSA`](@ref) data source.
-
-#### `CMIPphase`
-
-Can be either [`CMIP5`](@ref) or [`CMIP6`](@ref).
-
-#### `ClimateModel`
-
-Climate models can be chosen from: 
-
-`$(join(CMIP5_MODELS, "`,  `"))` for `CMIP5`;
-
-`$(join(CMIP6_MODELS, "`,  `"))` for `CMIP6`;"
-
-#### `ClimateScenario`
-
-CMIP5 Climate scenarios are all [`RepresentativeConcentrationPathway`](@ref)
-and can be chosen from: `RCP26`, `RCP45`, `RCP60`, `RCP85`
-
-CMIP6 Climate scenarios are all [`SharedSocioeconomicPathway`](@ref) and
-can be chosen from: `SSP126`, `SSP245`, `SSP370`, `SSP585`
-
-However, note that not all climate scenarios are available for all models.
-
-## Example
-
-```jldoctest future
-using RasterDataSources
-dataset = Future{BioClim, CMIP5, BNUESM, RCP45}
-# output
-Future{BioClim, CMIP5, BNUESM, RCP45}
-```
-Currently `Future` is only implented for `CHELSA`
-
-```jldoctest future
-datasource = CHELSA{Future{BioClim, CMIP5, BNUESM, RCP45}}
-```
-
-"""
 struct Future{D<:RasterDataSet,C<:CMIPphase,M<:ClimateModel,S<:ClimateScenario} end
 
 _dataset(::Type{<:Future{D}}) where D = D
